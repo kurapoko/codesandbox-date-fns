459ce49862d0f1b15ba2617afc3cbb25e004b071
kurapoko <kuraharamasahiro@gmail.com>
2022-10-17 03:28:10 +0000

    test fns method

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import "./styles.css";
+import { addDays, addHours, addMonths, subMinutes, subMonths } from "date-fns";
+
+const now = new Date("2020-03-31");
+const prevMonth = subMonths(now, 1);
 
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
+<h2>${now}</h2>
+<h2>${prevMonth}</h2>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
